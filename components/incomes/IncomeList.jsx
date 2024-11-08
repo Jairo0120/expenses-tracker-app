64e1d854f1a6c625e2dbfb3cf6c5e1f4f1c68c3c
Jairo Leyton <jali1993@hotmail.com>
2024-11-08 09:10:09 -0500

    Add cycle selector for income list

--- components/incomes/IncomeList.jsx
+++ components/incomes/IncomeList.jsx
@@ -5,6 +5,11 @@ import { showMessage } from "react-native-flash-message";
 import { useAuth0 } from "react-native-auth0";
 import { IncomeModalVisibleContext } from "../../contexts/incomes/IncomeModalVisibleContext";
 import IncomeCard from "./IncomeCard";
+import { CycleListContext } from "../../contexts/cycles/CycleListContext";
+import { CycleContext } from "../../contexts/cycles/CycleContext";
+import { formatShortDate } from "../../helpers/utils";
+import { Dropdown } from "react-native-element-dropdown";
+import { View, StyleSheet } from "react-native";
 
 export default function IncomeList({ refreshIncomes, setRefreshIncomes }) {
   const [incomes, setIncomes] = useState([]);
@@ -12,6 +17,34 @@ export default function IncomeList({ refreshIncomes, setRefreshIncomes }) {
   const [isListEnd, setIsListEnd] = useState(false);
   const { getCredentials } = useAuth0();
   const { modalVisible } = useContext(IncomeModalVisibleContext);
+  const { cycleList } = useContext(CycleListContext);
+  const { selectedCycle, setSelectedCycle } = useContext(CycleContext);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const dropdownStyle = StyleSheet.create({
+    itemText: {
+      fontSize: 14,
+      paddingVertical: 0,
+    },
+    dropdown: {
+      height: 35,
+      borderColor: "gray",
+      borderRadius: 8,
+      paddingHorizontal: 8,
+      paddingVertical: 8,
+      backgroundColor: "white",
+    },
+    selectedTextStyle: {
+      fontSize: 14,
+    },
+    placeholderStyle: {
+      fontSize: 14,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
 
   const refresh = async () => {
     setIsListEnd(false);
@@ -32,10 +65,11 @@ export default function IncomeList({ refreshIncomes, setRefreshIncomes }) {
       const response = await getIncomes(credentials.accessToken, {
         limit: 10,
         skip: skip,
+        ...(selectedCycle && { cycle_id: selectedCycle }),
       });
       if (response.status !== 200) {
         throw new Error(
-          `Error al obtener los ingresos desde el API. Status: ${response.status}`
+          `Error al obtener los ingresos desde el API. Status: ${response.status}`,
         );
       }
       setIncomes([...initialIncomes, ...response.data]);
@@ -54,6 +88,10 @@ export default function IncomeList({ refreshIncomes, setRefreshIncomes }) {
   };
 
   useEffect(() => {
+    refresh();
+  }, [selectedCycle]);
+
+  useEffect(() => {
     if (refreshIncomes) {
       setIsListEnd(false);
       setRefreshIncomes(false);
@@ -62,27 +100,56 @@ export default function IncomeList({ refreshIncomes, setRefreshIncomes }) {
   }, [refreshIncomes, fetchIncomes]);
 
   return (
-    <FlatList
-      className={`${modalVisible ? "opacity-10" : ""}`}
-      data={incomes}
-      onRefresh={refresh}
-      refreshing={isLoading}
-      keyExtractor={(income) => income.id.toString()}
-      renderItem={({ item }) => <IncomeCard income={item} />}
-      ListFooterComponent={
-        <Text className="text-white text-center pb-2">
-          {isListEnd && "No hay más ingresos qué mostrar"}
-        </Text>
-      }
-      ListEmptyComponent={
-        <Text className="text-white text-center pt-2">
-          {!isLoading && incomes.length === 0
-            ? "No hay ingresos qué mostrar"
-            : ""}
-        </Text>
-      }
-      onEndReachedThreshold={0.2}
-      onEndReached={loadMoreIncomes}
-    />
+    <View className="flex-col">
+      <View className="mb-3 mx-3 flex-row">
+        <Dropdown
+          style={[dropdownStyle.dropdown, isFocus && { borderColor: "blue" }]}
+          className="basis-2/6"
+          selectedTextStyle={dropdownStyle.selectedTextStyle}
+          inputSearchStyle={dropdownStyle.inputSearchStyle}
+          itemTextStyle={dropdownStyle.itemText}
+          itemContainerStyle={{ padding: 0, height: 55 }}
+          data={cycleList}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          searchPlaceholder="Buscar..."
+          placeholder={formatShortDate(new Date())}
+          placeholderStyle={dropdownStyle.placeholderStyle}
+          value={selectedCycle}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setSelectedCycle(item.value);
+            setIsFocus(false);
+          }}
+        />
+      </View>
+      <View>
+        <FlatList
+          className={`${modalVisible ? "opacity-10" : ""}`}
+          data={incomes}
+          onRefresh={refresh}
+          refreshing={isLoading}
+          keyExtractor={(income) => income.id.toString()}
+          renderItem={({ item }) => <IncomeCard income={item} />}
+          ListFooterComponent={
+            <Text className="text-white text-center pb-2">
+              {isListEnd && "No hay más ingresos qué mostrar"}
+            </Text>
+          }
+          ListEmptyComponent={
+            <Text className="text-white text-center pt-2">
+              {!isLoading && incomes.length === 0
+                ? "No hay ingresos qué mostrar"
+                : ""}
+            </Text>
+          }
+          onEndReachedThreshold={0.2}
+          onEndReached={loadMoreIncomes}
+        />
+      </View>
+    </View>
   );
 }
--- components/incomes/Main.jsx
+++ components/incomes/Main.jsx
@@ -4,6 +4,7 @@ import { styled } from "nativewind";
 import { useState } from "react";
 import { IncomeContext } from "../../contexts/incomes/IncomeContext";
 import { IncomeModalVisibleContext } from "../../contexts/incomes/IncomeModalVisibleContext";
+import { CycleContext } from "../../contexts/cycles/CycleContext";
 import IncomeModal from "./IncomeModal";
 import IncomeList from "./IncomeList";
 
@@ -13,29 +14,32 @@ export default function Main() {
   const [modalVisible, setModalVisible] = useState(false);
   const [refreshIncomes, setRefreshIncomes] = useState(true);
   const [selectedIncome, setSelectedIncome] = useState(null);
+  const [selectedCycle, setSelectedCycle] = useState(null);
 
   return (
     <IncomeContext.Provider value={{ selectedIncome, setSelectedIncome }}>
       <IncomeModalVisibleContext.Provider
         value={{ modalVisible, setModalVisible }}
       >
-        <View className="content-start">
-          <IncomeModal setRefreshIncomes={setRefreshIncomes} />
-          <IncomeList
-            refreshIncomes={refreshIncomes}
-            setRefreshIncomes={setRefreshIncomes}
-          />
-        </View>
-        <StyledPressable
-          className="absolute right-5 bottom-5 active:opacity-50"
-          onPress={() => setModalVisible(!modalVisible)}
-        >
-          <PlusIcon
-            size={60}
-            color="#81c1f8"
-            style={!modalVisible ? {} : { opacity: 0.1 }}
-          />
-        </StyledPressable>
+        <CycleContext.Provider value={{ selectedCycle, setSelectedCycle }}>
+          <View className="content-start">
+            <IncomeModal setRefreshIncomes={setRefreshIncomes} />
+            <IncomeList
+              refreshIncomes={refreshIncomes}
+              setRefreshIncomes={setRefreshIncomes}
+            />
+          </View>
+          <StyledPressable
+            className="absolute right-5 bottom-5 active:opacity-50"
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <PlusIcon
+              size={60}
+              color="#81c1f8"
+              style={!modalVisible ? {} : { opacity: 0.1 }}
+            />
+          </StyledPressable>
+        </CycleContext.Provider>
       </IncomeModalVisibleContext.Provider>
     </IncomeContext.Provider>
   );
