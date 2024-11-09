0c9ab0c42c7753b7ef8559a785993e009c6e763b
Jairo Leyton <jali1993@hotmail.com>
2024-11-09 08:20:24 -0500

    Add date cycle dropdown for savings

--- components/savings/Main.jsx
+++ components/savings/Main.jsx
@@ -4,6 +4,7 @@ import { styled } from "nativewind";
 import { useState } from "react";
 import { SavingContext } from "../../contexts/savings/SavingContext";
 import { SavingModalVisibleContext } from "../../contexts/savings/SavingModalVisibleContext";
+import { CycleContext } from "../../contexts/cycles/CycleContext";
 import SavingModal from "./SavingModal";
 import SavingList from "./SavingList";
 
@@ -13,29 +14,32 @@ export default function Main() {
   const [modalVisible, setModalVisible] = useState(false);
   const [refreshSavings, setRefreshSavings] = useState(true);
   const [selectedSaving, setSelectedSaving] = useState(null);
+  const [selectedCycle, setSelectedCycle] = useState(null);
 
   return (
     <SavingContext.Provider value={{ selectedSaving, setSelectedSaving }}>
       <SavingModalVisibleContext.Provider
         value={{ modalVisible, setModalVisible }}
       >
-        <View className="content-start">
-          <SavingModal setRefreshSavings={setRefreshSavings} />
-          <SavingList
-            refreshSavings={refreshSavings}
-            setRefreshSavings={setRefreshSavings}
-          />
-        </View>
-        <StyledPressable
-          className="absolute right-5 bottom-5 active:opacity-50"
-          onPress={() => setModalVisible(!modalVisible)}
-        >
-          <PlusIcon
-            size={60}
-            color="#81c1f8"
-            style={!modalVisible ? {} : { opacity: 0.1 }}
-          />
-        </StyledPressable>
+        <CycleContext.Provider value={{ selectedCycle, setSelectedCycle }}>
+          <View className="content-start">
+            <SavingModal setRefreshSavings={setRefreshSavings} />
+            <SavingList
+              refreshSavings={refreshSavings}
+              setRefreshSavings={setRefreshSavings}
+            />
+          </View>
+          <StyledPressable
+            className="absolute right-5 bottom-5 active:opacity-50"
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <PlusIcon
+              size={60}
+              color="#81c1f8"
+              style={!modalVisible ? {} : { opacity: 0.1 }}
+            />
+          </StyledPressable>
+        </CycleContext.Provider>
       </SavingModalVisibleContext.Provider>
     </SavingContext.Provider>
   );
--- components/savings/SavingList.jsx
+++ components/savings/SavingList.jsx
@@ -3,8 +3,13 @@ import { FlatList, Text } from "react-native";
 import { getSavings } from "../../api/savings";
 import { showMessage } from "react-native-flash-message";
 import { useAuth0 } from "react-native-auth0";
+import { formatShortDate } from "../../helpers/utils";
 import { SavingModalVisibleContext } from "../../contexts/savings/SavingModalVisibleContext";
+import { CycleListContext } from "../../contexts/cycles/CycleListContext";
+import { CycleContext } from "../../contexts/cycles/CycleContext";
 import SavingCard from "./SavingCard";
+import { View, StyleSheet } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function SavingList({ refreshSavings, setRefreshSavings }) {
   const [savings, setSavings] = useState([]);
@@ -12,6 +17,34 @@ export default function SavingList({ refreshSavings, setRefreshSavings }) {
   const [isListEnd, setIsListEnd] = useState(false);
   const { getCredentials } = useAuth0();
   const { modalVisible } = useContext(SavingModalVisibleContext);
+  const { cycleList } = useContext(CycleListContext);
+  const { selectedCycle, setSelectedCycle } = useContext(CycleContext);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const dropdownStyle = StyleSheet.create({
+    itemText: {
+      fontSize: 14,
+      paddingVertical: 0,
+    },
+    dropdown: {
+      height: 35,
+      borderColor: "gray",
+      borderRadius: 8,
+      paddingHorizontal: 8,
+      paddingVertical: 8,
+      backgroundColor: "white",
+    },
+    selectedTextStyle: {
+      fontSize: 14,
+    },
+    placeholderStyle: {
+      fontSize: 14,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
 
   const refresh = async () => {
     setIsListEnd(false);
@@ -32,10 +65,11 @@ export default function SavingList({ refreshSavings, setRefreshSavings }) {
       const response = await getSavings(credentials.accessToken, {
         limit: 10,
         skip: skip,
+        ...(selectedCycle && { cycle_id: selectedCycle }),
       });
       if (response.status !== 200) {
         throw new Error(
-          `Error al obtener los ahorros desde el API. Status: ${response.status}`
+          `Error al obtener los ahorros desde el API. Status: ${response.status}`,
         );
       }
       setSavings([...initialSavings, ...response.data]);
@@ -54,6 +88,10 @@ export default function SavingList({ refreshSavings, setRefreshSavings }) {
   };
 
   useEffect(() => {
+    refresh();
+  }, [selectedCycle]);
+
+  useEffect(() => {
     if (refreshSavings) {
       setIsListEnd(false);
       setRefreshSavings(false);
@@ -62,27 +100,56 @@ export default function SavingList({ refreshSavings, setRefreshSavings }) {
   }, [refreshSavings, fetchSavings]);
 
   return (
-    <FlatList
-      className={`${modalVisible ? "opacity-10" : ""}`}
-      data={savings}
-      onRefresh={refresh}
-      refreshing={isLoading}
-      keyExtractor={(saving) => saving.id.toString()}
-      renderItem={({ item }) => <SavingCard saving={item} />}
-      ListFooterComponent={
-        <Text className="text-white text-center pb-2">
-          {isListEnd && "No hay más ahorros qué mostrar"}
-        </Text>
-      }
-      ListEmptyComponent={
-        <Text className="text-white text-center pt-2">
-          {!isLoading && savings.length === 0
-            ? "No hay ahorros qué mostrar"
-            : ""}
-        </Text>
-      }
-      onEndReachedThreshold={0.2}
-      onEndReached={loadMoreSavings}
-    />
+    <View className="flex-col">
+      <View className="mb-3 mx-3 flex-row">
+        <Dropdown
+          style={[dropdownStyle.dropdown, isFocus && { borderColor: "blue" }]}
+          className="basis-2/6"
+          selectedTextStyle={dropdownStyle.selectedTextStyle}
+          inputSearchStyle={dropdownStyle.inputSearchStyle}
+          itemTextStyle={dropdownStyle.itemText}
+          itemContainerStyle={{ padding: 0, height: 55 }}
+          data={cycleList}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          searchPlaceholder="Buscar..."
+          placeholder={formatShortDate(new Date())}
+          placeholderStyle={dropdownStyle.placeholderStyle}
+          value={selectedCycle}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item) => {
+            setSelectedCycle(item.value);
+            setIsFocus(false);
+          }}
+        />
+      </View>
+      <View>
+        <FlatList
+          className={`${modalVisible ? "opacity-10" : ""}`}
+          data={savings}
+          onRefresh={refresh}
+          refreshing={isLoading}
+          keyExtractor={(saving) => saving.id.toString()}
+          renderItem={({ item }) => <SavingCard saving={item} />}
+          ListFooterComponent={
+            <Text className="text-white text-center pb-2">
+              {isListEnd && "No hay más ahorros qué mostrar"}
+            </Text>
+          }
+          ListEmptyComponent={
+            <Text className="text-white text-center pt-2">
+              {!isLoading && savings.length === 0
+                ? "No hay ahorros qué mostrar"
+                : ""}
+            </Text>
+          }
+          onEndReachedThreshold={0.2}
+          onEndReached={loadMoreSavings}
+        />
+      </View>
+    </View>
   );
 }
