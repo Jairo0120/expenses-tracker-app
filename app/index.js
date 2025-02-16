bac8b5050d911475e66c495987e11e26ea52ed3b
Jairo Leyton <jali1993@hotmail.com>
2025-02-16 18:17:21 -0500

    Add button to login in the index page

--- app/index.js
+++ app/index.js
@@ -1,30 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Button } from "react-native";
 import { useEffect } from "react";
 import { useAuth0 } from "react-native-auth0";
 import { ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function Index() {
-  const { isLoading, authorize, getCredentials } = useAuth0();
+  const { isLoading, authorize, getCredentials, user } = useAuth0();
   const router = useRouter();
 
-  useEffect(() => {
-    const authorizer = async () => {
-      console.log("index.js: authorizer");
-      try {
-        const currentCredentials = await getCredentials();
-        if (!currentCredentials) {
-          await authorize({
-            audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
-            nonce: process.env.EXPO_PUBLIC_AUTH0_NONCE,
-            scope: "openid profile email offline_access",
-          });
-        }
-        router.replace("/app/expenses");
-      } catch (e) {
-        console.error(e);
+  const authorizer = async () => {
+    console.log("index.js: authorizer");
+    try {
+      const currentCredentials = await getCredentials();
+      if (!currentCredentials) {
+        await authorize({
+          audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
+          nonce: process.env.EXPO_PUBLIC_AUTH0_NONCE,
+          scope: "openid profile email offline_access",
+        });
       }
-    };
+      const newCredentials = await getCredentials();
+      if (newCredentials) {
+        router.replace("/app/expenses");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
     if (!isLoading) {
       authorizer();
     }
@@ -33,6 +37,9 @@ export default function Index() {
   return (
     <View className="items-center justify-center flex-1">
       {isLoading && <ActivityIndicator />}
+      {!isLoading && (user == null || user !== undefined) && (
+        <Button onPress={authorizer} title="Iniciar sesión" />
+      )}
     </View>
   );
 }
